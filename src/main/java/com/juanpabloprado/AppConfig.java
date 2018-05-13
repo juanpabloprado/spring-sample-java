ca627def3b36b6534d134e587d5dcca31581acbd
Juan Pablo Prado <juan@juanpabloprado.com>
2018-05-13 14:51:38 -0500

    Initial commit

---
+++ src/main/java/com/juanpabloprado/AppConfig.java
@@ -0,0 +1,31 @@
+package com.juanpabloprado;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@ComponentScan({"com.juanpabloprado"})
+@PropertySource("app.properties")
+public class AppConfig {
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+	
+	//@Bean(name = "customerService")
+	//public CustomerService getCustomerService() {
+		//CustomerServiceImpl customerService = new CustomerServiceImpl(getCustomerRepository());
+		//CustomerServiceImpl customerService = new CustomerServiceImpl();
+		//customerService.setCustomerRepository(getCustomerRepository());
+		//return customerService;
+	//}
+	
+	//@Bean(name = "customerRepository")
+	//public CustomerRepository getCustomerRepository() {
+	//	return new HibernateCustomerRepositoryImpl();
+	//}
+}
---
+++ src/main/java/com/juanpabloprado/HibernateCustomerRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.juanpabloprado;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+@Repository("customerRepository")
+public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+	
+	@Value("${dbusername}")
+	private String dbUsername;
+	
+	/* (non-Javadoc)
+	 * @see com.juanpabloprado.CustomerRepository#findAll()
+	 */
+	@Override
+	public List<Customer> findAll(){
+		System.out.println(dbUsername);
+		
+		List<Customer> customers = new ArrayList<>();
+		
+		Customer customer = new Customer();
+		
+		customer.setFirstName("Juan");
+		customer.setLastName("Prado");
+		customers.add(customer);
+		
+		return customers;
+	}
+}
+
